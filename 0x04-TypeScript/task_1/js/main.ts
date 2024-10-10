d1097440ac1664cdbd63bbd533e2cefdc847fc15
giftdavid101 <giftdavid101@gmail.com>
2024-10-10 03:49:31 +0100

    main.ts

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,34 @@
+export interface TeacherAttributes {
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  contract: false,
+  [index:string]: any,
+}
+class Teacher implements TeacherAttributes {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+
+  constructor(
+    firstName: string,
+    lastName: string,
+    fullTimeEmployee: boolean,
+    location: string,
+    attributes?: TeacherAttributes
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullTimeEmployee = fullTimeEmployee;
+    this.location = location;
+    if (attributes) {
+      Object.assign(this, attributes);
+    }
+  }
+}
+

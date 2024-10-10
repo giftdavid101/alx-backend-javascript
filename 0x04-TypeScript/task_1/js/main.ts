c8ced4962a92f97500b8c0f8399daa3fbea5bc5e
giftdavid101 <giftdavid101@gmail.com>
2024-10-10 04:03:51 +0100

    added student class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,7 +14,13 @@ export interface Director extends TeacherAttributes {
 export interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
-
+export interface StudentClassContructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
 class Teacher implements TeacherAttributes {
   readonly firstName: string;
   readonly lastName: string;
@@ -41,4 +47,22 @@ class Teacher implements TeacherAttributes {
 }
 export function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
+}
+
+export class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this._firstName;
+  }
 }
